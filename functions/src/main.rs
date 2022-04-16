4693fd02615f7787f43cc43c8bb9161ffd9d4972
Johnathan Middleton <johnathanmiddleton1@gmail.com>
2022-04-16 18:29:26 -0400

    functions

---
+++ functions/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("Hello, world!");
+
+    let result = another_function(5, 6);
+
+    println!("The result is {}", result);
+}
+
+fn another_function(x: i32, y: i32) -> i32 {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+
+    11
+}
